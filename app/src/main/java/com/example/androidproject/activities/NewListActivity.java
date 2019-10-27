fe55ecc498c249a44c4c2352a173d7032bb0371c
lyndaflower <umurerwalynda@gmail.com>
2019-10-27 13:26:09 +0200

    add another new class called perfumes

--- app/src/main/java/com/example/androidproject/activities/NewListActivity.java
+++ app/src/main/java/com/example/androidproject/activities/NewListActivity.java
@@ -1,4 +1,10 @@
 package com.example.androidproject.activities;
 
-public class NewListActivity {
+import android.content.SharedPreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewListActivity extends AppCompatActivity {
+    private SharedPreferences sharedpref;
+    private String mAdresses;
 }
---
+++ app/src/main/java/com/example/androidproject/models/Perfume.java
@@ -0,0 +1,4 @@
+package com.example.androidproject.models;
+
+public class Perfume {
+}
