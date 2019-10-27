de2cf17a6d888b41f3e5ebfc4cd6d31c54803026
lyndaflower <umurerwalynda@gmail.com>
2019-10-27 13:38:32 +0200

    Add adapter

--- app/src/main/java/com/example/androidproject/activities/NewListActivity.java
+++ app/src/main/java/com/example/androidproject/activities/NewListActivity.java
@@ -1,10 +1,43 @@
 package com.example.androidproject.activities;
 
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidproject.R;
+import com.example.androidproject.adapters.listAdapter;
+import com.example.androidproject.models.Business;
+import com.example.androidproject.models.Perfume;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NewListActivity extends AppCompatActivity {
     private SharedPreferences sharedpref;
     private String mAdresses;
+
+    private RecyclerView mRecycle;
+    private ProgressBar mProgressText;
+    private TextView mErrorView;
+
+    private listAdapter mAdapter;
+    private List<Perfume> mSpray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final);
+
+        mRecycle = (RecyclerView) findViewById(R.id.recyclerView);
+        mProgressText = (ProgressBar) findViewById(R.id.progressBar);
+        mErrorView = (TextView) findViewById(R.id.errorView);
+
+        ButterKnife.bind(this);
+    }
 }
